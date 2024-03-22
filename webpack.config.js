7f590b2fb4addd3201fd1b80c77b609036e3f5c8
duker22 <dukin.misha22@gmail.com>
2024-03-22 20:36:25 +0300

    settings webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,49 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
+module.exports = {
+  devtool: "source-map",
+  entry: "./src/index.js",
+  mode: process.env.NODE_ENV || "development",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: [
-          process.env.NODE_ENV === 'production'
-            ? MiniCssExtractPlugin.loader
-            : 'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.scss$/,
         use: [
-          process.env.NODE_ENV === 'production'
-            ? MiniCssExtractPlugin.loader
-            : 'style-loader',
-          'css-loader',
-          'sass-loader',
-          'postcss-loader',
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
         ],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
+        use: "url-loader?limit=10000",
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: "file-loader",
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
     new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
   ],
-  output: {
-    clean: true,
-  },
 };
